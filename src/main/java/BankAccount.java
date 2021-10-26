82c56ba10ee444995391108bce4010a0378a5112
savonnahwong <23swong@athenian.org>
2021-10-26 10:41:39 -0700

    not finished

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,4 +3,40 @@ public class BankAccount {
     private int balance;
     private int accountNumber;
     private String preferredName;
+
+
+    public BankAccount (int b, int a, String p){
+        balance = b;
+        accountNumber = a;
+        preferredName = p;
+    }
+
+    public void Account (){
+        this.balance = 0;
+    }
+
+    public void deposit (int input){
+        this.balance = balance + input;
+    }
+
+    public void withdraw (int input){
+        this.balance = balance - input;
+    }
+
+    public void getAccountname (String name){
+        this.preferredName = name;
+    }
+
+    public int getBalance (){
+        return balance;
+    }
+
+    public int getAccountnum(){
+        return (101134);
+    }
+
+    @Override
+    public String toString() {return "Account" +  this.accountNumber + "has a balance of: " + this.balance;}
+
+
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -6,7 +6,18 @@ public class BankClient {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your preferred name?");
         String name = scan.nextLine();
-//        // Create a bank account with the given name and the account number 101134
+        System.out.println("Hi, "+name+"! What command would you like to carry out? (withdraw/deposit/check balance/check account number/set preferred name/print summary)");
+        String command = scan.nextLine();
+        BankAccount account = new BankAccount(0,101134, name);
+        if(command.equals("withdraw")){
+            System.out.println("How much money would you like to withdraw?");
+            int howmuch = scan.nextInt();
+            account.withdraw(howmuch);
+        }
+        else if(command.equals("summary") ){
+            System.out.println(account);
+        }
+        // Create a bank account with the given name and the account number 101134
 //        BankAccount account = new BankAccount(name, 101134);
     }
 }
